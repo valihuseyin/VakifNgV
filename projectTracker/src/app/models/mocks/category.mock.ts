6a133d6fd7cf7c14582d0d8bbf008e57b092739a
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-01-13 10:33:31 +0300

    creating model and mocks

---
+++ projectTracker/src/app/models/mocks/category.mock.ts
@@ -0,0 +1,7 @@
+import { Category } from "../category.model";
+
+export const categories: Category[] = [
+    new Category(1,"Çağrı Merkezi"),
+    new Category(2,"Bireysel Bankacılık"),
+    new Category(3,"Mobil Uygulamalar")
+];
