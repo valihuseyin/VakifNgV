e0e17eff46d1c4c9452406a74b723e46a3027531
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-01-13 10:20:47 +0300

    components generated

--- projectTracker/src/app/app.module.ts
+++ projectTracker/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderMenuComponent } from './header-menu/header-menu.component';
+import { CategoryMenuComponent } from './category-menu/category-menu.component';
+import { ProjectListComponent } from './project-list/project-list.component';
+import { ProjectComponent } from './project/project.component';
+import { TaskListComponent } from './task-list/task-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderMenuComponent,
+    CategoryMenuComponent,
+    ProjectListComponent,
+    ProjectComponent,
+    TaskListComponent
   ],
   imports: [
     BrowserModule,
